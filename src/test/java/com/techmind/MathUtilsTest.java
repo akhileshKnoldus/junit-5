a6336756dcf6b32c47e18cb9aca841fc69dfaf33
Akhilesh Kumar Gupta <akhilesh.gupta10011996@gmail.com>
2020-08-26 00:41:18 +0530

    used of @Nested annotation

--- src/test/java/com/techmind/MathUtilsTest.java
+++ src/test/java/com/techmind/MathUtilsTest.java
@@ -7,8 +7,10 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("When running MathUtils")
 class MathUtilsTest {
 
 	MathUtils mathUtils;
@@ -28,19 +30,24 @@ class MathUtilsTest {
 		System.out.println("Cleaning up....");
 	}
 	
-	@Test
-	@DisplayName("Addition for two number")
-	void testAddIntInt() {
-		int firstNum = 10;
-		int secondNum = 20;
-		assertEquals(30 ,MathUtils.add(firstNum, secondNum),  "This method is should be add two numbers");
-	}
+	@Nested
+	@DisplayName("add method")
+	class AddTest {
 
-	@Test
-	@DisplayName("Addition for an array")
-	void testAddIntArray() {
-		int[] arr = new int[] {1,2,3,4,5,6,7,8,9,10};
-		assertEquals(55 ,MathUtils.add(arr) ,"Addition of an array");
+		@Test
+		@DisplayName("Addition for two number")
+		void testAddIntInt() {
+			int firstNum = 10;
+			int secondNum = 20;
+			assertEquals(30, MathUtils.add(firstNum, secondNum), "This method is should be add two numbers");
+		}
+
+		@Test
+		@DisplayName("Addition for an array")
+		void testAddIntArray() {
+			int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+			assertEquals(55, MathUtils.add(arr), "Addition of an array");
+		}
 	}
 	
 	@Test
